e05536f6a7b59700f83ab82c8dd8a96940ad5879
EasterFan <fan.easter@gmail.com>
2017-11-19 11:35:37 +0800

    创建线程--实现Runnable接口

---
+++ ThreadProj/src/RunnableTest.java
@@ -0,0 +1,35 @@
+/**
+ * 实现Runnable接口创建线程类
+ */
+
+
+class PrintRunnable implements Runnable{
+
+    
+    @Override
+    public void run() {
+        int i = 0;
+        while (i<5) {
+            // 只能调用Thread类的静态方法
+            System.out.println(Thread.currentThread().getName() + "正在运行"+(i++));
+            // i++与++i的区别在这里显现
+        }
+    }
+}
+
+public class RunnableTest {
+    public static void main(String[] args) {
+        // 启动线程三步
+        // 1.新建自定义线程类对象
+        PrintRunnable printRunnable1 = new PrintRunnable();
+        PrintRunnable printRunnable2 = new PrintRunnable();
+
+        // 2.将自定义线程类对象作为参数，新建Tread线程对象
+        Thread thread1 = new Thread(printRunnable1);
+        Thread thread2 = new Thread(printRunnable2);
+
+        // 3.通过Thread类的start方法启动线程
+        thread1.start();
+        thread2.start();
+    }
+}
