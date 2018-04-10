b263f3132700b1614838945e34b2c1963da4070a
EasterFan <fan.eastor@gmail.com>
2018-04-10 17:35:01 +0800

    add generic class

--- README.md
+++ README.md
@@ -52,6 +52,7 @@ JDBC
 
 2017年11月18日 [Map存储自定义类--商品类](MapProj/src/MapGoodsTest.java)  
 2017年11月18日 [Map存储String类--zoo字典](MapProj/src/MapZoo.java)  
+2018年04月10日 [泛型类 -- 工具类编译时报错](SetProj/src/generic/GenericClass.java)  
 2018年04月10日 [泛型的使用 -- 省略上下转型](SetProj/src/generic/StringTreeSetWithoutClassCast.java)  
 2018年04月10日 [TreeSet存储字符串 -- 根据字符串长度排序-集合比较性](SetProj/src/treeset/StringTreeSetDemo.java)  
 2018年04月10日 [TreeSet存储学生类 -- 实现Comparator接口集合具备比较性](SetProj/src/treeset/TreeSetOrderByCollection.java)  
---
+++ SetProj/src/generic/GenericClass.java
@@ -0,0 +1,62 @@
+package generic;
+
+/**
+ * 泛型类
+ */
+public class GenericClass {
+    public static void main(String[] args) {
+
+        // 以前的出现类型转换异常的写法 - 编译时不报错, 运行时报错
+        /*
+        Tool tool = new Tool();
+        tool.setObj(new Student());
+        Worker wo = (Worker) tool.getObject();
+        */
+
+        // 现在的泛型写法  -- 编译时报错
+        GenericTool<Worker> workerGenericTool = new GenericTool<Worker>();
+        workerGenericTool.setAnimal(new Student());
+        workerGenericTool.setAnimal(new Worker());
+    }
+}
+
+/**
+ * 以前的实现方法
+ */
+
+/*
+class Tool{
+    private Object obj;
+
+    public void setObj(Object obj){
+        this.obj = obj;
+    }
+    public Object getObject(){
+        return obj;
+    }
+}
+
+*/
+
+/**
+ * 现在的泛型写法  -- 泛型类
+ */
+class GenericTool<Animal>{
+    private Animal animal;
+
+    public void setAnimal(Animal animal){
+        this.animal = animal;
+    }
+
+    public Animal getAnimal(){
+        return animal;
+    }
+}
+
+class Student{
+
+}
+
+class Worker{
+
+}
